c1585a3a712b753c1e5aa6726833dd88daea8b9a
ZeynabAliyeva <zeynabaliyeva1783@gmail.com>
2023-12-19 16:36:21 +0400

    first component select organization

--- src/app/onboarding/select-organization/page.tsx
+++ src/app/onboarding/select-organization/page.tsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import { Button, Select, Stack } from "@chakra-ui/react";
+import "../../globals.css";
 
-const SelectOrganization = () => {
+function Page() {
   return (
-    <div>SelectOrganization</div>
-  )
-}
+    <div className="container">
+      <h1 className="title">Təşkilatı seçin</h1>
+      <p className="subTitle">
+        Qeydiyyatdan keçirmək istədiyiniz şirkəti seçməniz xahiş olunur
+      </p>
 
-export default SelectOrganization
+      <div className="selectContainer">
+        <Stack spacing={3}>
+          <Select size="md">
+            <option value="option1" className="option_first">
+              <div >Teşkilatın adı</div>
+              <div>Teşkiilatın VÖEN-i</div>
+            </option>
+          </Select>
+        </Stack>
+      </div>
+
+      <div className="buttonContainer">
+        <Button colorScheme="blue">Davam et </Button>
+
+        <Button
+          size="md"
+          height="42px"
+          width="500px"
+          border="2px"
+          borderColor="transparent"
+        >
+          İmtina et
+        </Button>
+      </div>
+    </div>
+  );
+}
+export default Page;
