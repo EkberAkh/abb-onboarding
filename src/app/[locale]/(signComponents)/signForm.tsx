b0f5d7e62c3f6c8cdaafaa333af61083b1410bae
EkberAkh <ekber.0303@gmail.com>
2023-12-23 18:58:03 +0400

    add disabled functionality to button

--- src/app/[locale]/(signComponents)/signForm.tsx
+++ src/app/[locale]/(signComponents)/signForm.tsx
@@ -1,14 +1,31 @@
-"use client"
-import { VStack,FormControl, FormLabel, Input, FormErrorMessage, Text, useToast,InputGroup,InputLeftAddon} from "@chakra-ui/react"
+"use client";
+import {
+  VStack,
+  FormControl,
+  FormLabel,
+  Input,
+  FormErrorMessage,
+  Text,
+  useToast,
+  InputGroup,
+  InputLeftAddon,
+  Box,
+  Button,
+  Checkbox,
+  Link,
+} from "@chakra-ui/react";
 import { Controller, Form, useForm } from "react-hook-form";
 import { useState } from "react";
 
 const SignForm = () => {
+  const [isChecked, setIsChecked] = useState(false);
+  const [isInputValid, setIsInputValid] = useState(false);
 
   const {
     control,
     handleSubmit,
     formState: { errors },
+    getValues,
   } = useForm({
     mode: "all",
     defaultValues: {
@@ -17,114 +34,134 @@ const SignForm = () => {
     },
   });
 
-  const [inputTextValue, setinputTextValue] = useState("");
-
-  const [inputNumberValue, setInputNumberValue] = useState("");
-
-  const submitFunc = (value: any) => {
-    console.log(value);
+  const handleCheckboxChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ): void => {
+    setIsChecked(event.target.checked);
+    checkFormValidity();
   };
 
-  // const toast = useToast()
+  const handleInputChange = () => {
+    checkFormValidity();
+  };
 
-    return (
-        <VStack  gap='24px' w='100%'>
-          <form 
-            style={{ width: "100%" }} 
-            onSubmit={handleSubmit(submitFunc)} 
-            // onClick={() =>
-            //   toast({
-            //     title: 'alert',
-            //     description: "Verilən telefon nömrəsi və ya istifadəçi ID-si düzgün deyil.",
-            //     status: 'error',
-            //     duration: 5000,
-            //     isClosable: true,
-            //   })
-            // }
-          >
-          <FormControl  gap='8px' mb='24px' isInvalid={!!errors?.phoneNumber}>
-            <FormLabel >ASAN İmza Mobil nömrə</FormLabel>
-            <InputGroup w="100%">
-                <InputLeftAddon color="gray.700" bg="gray.100">
-                  +994
-                </InputLeftAddon>
-                <Controller
-                  name="phoneNumber"
-                  control={control}
-                  rules={{
-                    required: "Asan ID qeyd olunmayıb",
-                    validate: (value: string) => {
-                      if (value.length !== 9) {
-                        return "Asan ID düzgün deyil";
-                      }
-                      return undefined;
-                    },
-                  }}
-                  render={({ field }) => (
-                    <Input
-                      {...field}
-                      borderLeft="0"
-                      borderTopLeftRadius={0}
-                      borderBottomLeftRadius={0}
-                      type="tel"
-                      placeholder="00 000 00 00"
-                      borderColor="gray.300"
-                      w="100%"
-                      maxLength={9}
-                      onChange={(e) => {
-                        field.onChange(e);
-                        setinputTextValue(field.value);
-                      }}
-                    />
-                  )}
-                />
-              </InputGroup>
-            <FormErrorMessage mt="0.5rem">
-                {errors?.phoneNumber?.message}
-              </FormErrorMessage>
-          </FormControl>
-          <FormControl gap='8px'>
-            <FormLabel>ASAN İmza İstifadəçi ID-si</FormLabel>
+  const checkFormValidity = () => {
+    const values = getValues();
+    const isPhoneValid = !!values.phoneNumber && values.phoneNumber.length === 9;
+    const isPasswordValid = !!values.password && values.password.length === 6;
+    setIsInputValid(isPhoneValid && isPasswordValid);
+  };
+  
+
+  const submitFunc = () => {
+    console.log(getValues("phoneNumber"));
+    console.log(getValues("password"));
+  };
+
+  return (
+    <VStack gap="24px" w="100%">
+      <form style={{ width: "100%" }} onSubmit={handleSubmit(submitFunc)}>
+        <FormControl gap="8px" mb="24px" isInvalid={!!errors?.phoneNumber}>
+          <FormLabel>ASAN İmza Mobil nömrə</FormLabel>
+          <InputGroup w="100%">
+            <InputLeftAddon color="gray.700" bg="gray.100">
+              +994
+            </InputLeftAddon>
             <Controller
-        name="password"
-        control={control}
-        rules={{
-          required: "Asan ID qeyd olunmayıb",
-          validate: (value: string) => {
-            if (value.length !== 6) {
-              return "Asan ID düzgün deyil";
-            }
-          },
-        }}
-        render={({ field }) => (
-          <Input
-            {...field}
-            value={field.value}
-            borderColor="gray.300"
-            _invalid={{ borderColor: "red.500" }}
-            placeholder="000000"
-            isInvalid={!!errors.password}
-            type="number"
-            w="100%"
-            onChange={(e) => {
-              if (e.target.value.length <= 6) {
-                field.onChange(e);
-                setInputNumberValue(field.value);
-              }
+              name="phoneNumber"
+              control={control}
+              rules={{
+                required: "Asan ID qeyd olunmayıb",
+                validate: (value: string) => {
+                  if (value.length !== 9) {
+                    return "Asan ID düzgün deyil";
+                  }
+                  return undefined;
+                },
+              }}
+              render={({ field }) => (
+                <Input
+                  {...field}
+                  borderLeft="0"
+                  borderTopLeftRadius={0}
+                  borderBottomLeftRadius={0}
+                  type="tel"
+                  placeholder="00 000 00 00"
+                  borderColor="gray.300"
+                  w="100%"
+                  maxLength={9}
+                  onChange={(e) => {
+                    field.onChange(e);
+                    handleInputChange();
+                  }}
+                />
+              )}
+            />
+          </InputGroup>
+          <FormErrorMessage mt="0.5rem">
+            {errors?.phoneNumber?.message}
+          </FormErrorMessage>
+        </FormControl>
+        <FormControl mb="24px" gap="8px">
+          <FormLabel>ASAN İmza İstifadəçi ID-si</FormLabel>
+          <Controller
+            name="password"
+            control={control}
+            rules={{
+              required: "Asan ID qeyd olunmayıb",
+              validate: (value: string) => {
+                if (value.length !== 6) {
+                  return "Asan ID düzgün deyil";
+                }
+              },
             }}
+            render={({ field }) => (
+              <Input
+                {...field}
+                value={field.value}
+                borderColor="gray.300"
+                _invalid={{ borderColor: "red.500" }}
+                placeholder="000000"
+                isInvalid={!!errors.password}
+                type="number"
+                w="100%"
+                onChange={(e) => {
+                  if (e.target.value.length <= 6) {
+                    field.onChange(e);
+                    handleInputChange();
+                  }
+                }}
+              />
+            )}
           />
-        )}
-      />
-      {errors?.password && (
-        <Text color="red" fontSize="sm" mt="0.5rem">
-          {errors?.password?.message}
-        </Text>
-      )}
-          </FormControl>
-          </form>
-            
-        </VStack>
-    )
-}
+          {errors?.password && (
+            <Text color="red" fontSize="sm" mt="0.5rem">
+              {errors?.password?.message}
+            </Text>
+          )}
+        </FormControl>
+        <Checkbox mb="20px" isChecked={isChecked} onChange={handleCheckboxChange}>
+          {" "}
+          Mən{" "}
+          <Link href={"az/terms-and-conditions"}>qaydalar və şərtlərlə </Link>
+          razıyam
+        </Checkbox>
+        <Box w="100%">
+          <Button
+            type="submit"
+            w="100%"
+            p="8px 16px"
+            bg="blue.300"
+            color="white"
+            isDisabled={!isChecked || !isInputValid}
+            fontSize="16px"
+          >
+            Daxil ol
+          </Button>
+        </Box>
+      </form>
+    </VStack>
+  );
+};
 
-export default SignForm
+export default SignForm;
--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { VStack, Image, Button, Box, FormControl, Text, FormLabel, Input, Checkbox, HStack } from "@chakra-ui/react";
 import Link from "next/link";
 import SignForm from "./(signComponents)/signForm";
@@ -7,10 +9,11 @@ import SignFooter from "./(signComponents)/signFooter";
 
 export default function Home() {
   
+  
   return (
-    <VStack  height='100vh' padding='10px' width='100%' justifyContent='space-between'>
+    <VStack  padding='10px' width='100%' justifyContent='space-between'>
       <Button  alignSelf='flex-end' border='none' bg='gray.100' color='gray.700' p='8px'>x</Button>
-      <VStack gap='14px' padding='12px' alignItems='flex-start' justifyContent='center'>
+      <VStack minWidth='465px' gap='14px' padding='12px' alignItems='flex-start' justifyContent='center'>
         <Box >
           <Image src="./images/abb-business.png"/>
         </Box>
@@ -27,15 +30,8 @@ export default function Home() {
               >Qeydiyyatdan keçmək üçün Asan İmza ilə daxil olun.</Text>
             </VStack>
             <SignForm/>
-          <Checkbox> Mən <Link  href={"az/terms-and-conditions"}>qaydalar və şərtlərlə </Link>razıyam</Checkbox>
-          <Box w='100%'>
-            <Button 
-            w='100%'
-            p='8px 16px' 
-            bg='gray.300' 
-            color='white' 
-            fontSize='16px'>Daxil ol</Button>
-          </Box>
+         
+       
       </VStack>
       <SignFooter/>
     </VStack>
