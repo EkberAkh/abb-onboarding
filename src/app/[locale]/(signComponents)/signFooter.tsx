9c5be06dac60cfa6c836b9facdf6f45ebf4312ab
taliieva <teranealieva01@gmail.com>
2023-12-24 23:41:49 +0400

    pass to the how to get asan imza

--- src/app/[locale]/(signComponents)/signFooter.tsx
+++ src/app/[locale]/(signComponents)/signFooter.tsx
@@ -1,10 +1,21 @@
 import { HStack, Image , Link} from "@chakra-ui/react"
+import { usePathname } from "next/navigation";
 
 const SignFooter = () => {
+    const pathName = usePathname();
+    const getUrl = () => {
+        if (pathName === "/az") {
+          return "https://asanimza.az/";
+        } else if (pathName === "/en") {
+          return "https://asanimza.az/en/";
+        } else {
+          return "https://asanimza.az/";
+        }
+      };
     return (
         <HStack p='24px 32px' gap='35px' alignItems='center'>
             <Image src="./images/Frame 7508.png"></Image>
-            <Link href="/">ASAN İmza necə alınmalı?</Link>
+            <Link href={getUrl()} target="_blank" _hover={{ color: 'blue' }}>ASAN İmza necə alınmalı?</Link>
       </HStack>
     )
 }
